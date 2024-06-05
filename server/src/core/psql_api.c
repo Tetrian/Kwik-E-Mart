b1061c2f4d31e4bf1048c0ba37bdfeaec13c6f19
Tetrian <cerronesimone97@gmail.com>
2024-06-05 12:32:31 +0200

    added db initialization function

--- server/src/core/psql_api.c
+++ server/src/core/psql_api.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdbool.h>
 #include <pthread.h>
 
@@ -12,36 +13,59 @@
  * Make a connection to the database server and create tables
 https://www.postgresql.org/docs/8.1/libpq.html#LIBPQ-CONNECT
 */
-// TODOS: restituire la struct
-PGconn *init_db(const char *conninfo) {
-  // Create the connection
-  PGconn *conn = PQconnectdb(conninfo);
-  if (PQstatus(conn) != CONNECTION_OK) {
-    log_error("[%s] (%s) Connection to database failed: %s", __FILE_NAME__,
-              __func__, PQerrorMessage(conn));
-    PQfinish(conn);
+db_t *init_db(const char *conninfo) {
+  db_t *db = malloc(sizeof(db_t));
+  if (db == NULL) {
+    log_error("[%s] (%s) Failed to allocate space for the db.\n",
+              __FILE_NAME__, __func__);
     return NULL;
   }
-  
-  //TODO:inizializza la struttura
+
+  // Create the connection
+  db->conn = PQconnectdb(conninfo);
+  if (PQstatus(db->conn) != CONNECTION_OK) {
+    log_error("[%s] (%s) Connection to database failed: %s", __FILE_NAME__,
+              __func__, PQerrorMessage(db->conn));
+    PQfinish(db->conn);
+    free(db);
+    return NULL;
+  }
+
+  // init mutex
+  if (pthread_mutex_init(&(db->mutex), NULL) != 0) {
+    log_error("[%s] (%s) Could not initializate the mutex! Cause: %s\n", 
+              __FILE_NAME__, __func__, strerror(errno));
+    perror("pthread_mutex_init: ");
+		PQfinish(db->conn);
+    free(db);
+    return NULL;
+  }
 
   // Create tables
-  if (!create_table(conn, CRT_PRODUCT_TBL))
+  if (!create_table(db->conn, CRT_PRODUCT_TBL)) {
+    destroy_db(db);
     return NULL;
+  }
 
-  // popola la tabella prodotti se non lo è già
-  // N.B.: usare e gestire un bool che te lo dica
-
-  if (!create_table(conn, CRT_RECEIPT_TBL))
+  if (!create_table(db->conn, CRT_RECEIPT_TBL)) {
+    destroy_db(db);
     return NULL;
+  }
 
-  return conn;
+  // population of table product
+  log_info("[%s] (%s) Starting populate the products table...\n",
+           __FILE_NAME__, __func__);
+  populate_product_tbl(db);
+
+  return db;
 }
 
-/** exit nicely */
-void cleanup_db(PGconn *conn, PGresult *res) {
-  PQclear(res);
-  PQfinish(conn);
+void destroy_db(db_t *db) {
+  if (db != NULL) {
+    PQfinish(db->conn);
+    pthread_mutex_destroy(&(db->mutex));
+    free(db);
+  }
 }
 
 /** create a table for the database */
@@ -50,9 +74,63 @@ bool create_table(PGconn *conn, const char *create_cmd) {
   if (PQresultStatus(res) != PGRES_COMMAND_OK) {
     log_error("[%s] (%s) CREATE failed: %s",__FILE_NAME__,
               __func__, PQerrorMessage(conn));
-    cleanup_db(conn, res);
+    PQclear(res);
     return false;
   }
   PQclear(res);
   return true;
 }
+
+/** populate the product table */
+void populate_product_tbl(db_t * db) {
+  // declare products name
+  const char *const names[] = {"Duff Beer", "KrustyO's", "Pizza", 
+                  "Donuts", "Buzz Cola", "Slurpee", "Pollo"};
+  size_t size = 7;
+
+  for (size_t i = 0; i < size; ++i) {
+    // generate random price
+    float price = (float)rand()/((float)RAND_MAX/PRICE_MAX);
+
+    insert(db, INSERT_PRODUCT, i+1, names[i], price);
+  }
+}
+
+
+/*
+ * insert a element into a database
+ * @param db database struct
+ * @param cmd insert command to be executed
+ * @param id object unic id
+ * @param str name of the product or date of the receipt
+ * @param price the price of product or total price of receipt
+ */
+void insert(db_t *db, const char *cmd, const int id,
+            const char *str, const float price) 
+{ 
+  // Parsing the id and the price
+  char cid[ENOUGHT];
+  sprintf(cid, "%d", id);
+  char cprice[ENOUGHT];
+  sprintf(cprice, "%.2f", (double)price);
+
+  // Specific the values and lenghts of parameters
+  const char *const param_values[] = {cid, str, cprice};
+  const int param_lengths[] = {sizeof(cid), sizeof(str), sizeof(cprice)};
+
+  pthread_mutex_lock(&(db->mutex));
+
+  // https://www.postgresql.org/docs/current/libpq-exec.html#LIBPQ-PQEXECPARAMS
+  PGresult *res = PQexecParams(db->conn, cmd, N_PARAMS,
+                      NULL, param_values, param_lengths, 
+                      NULL, TEXT_FORMAT
+  );
+
+	if (PQresultStatus(res) != PGRES_COMMAND_OK)
+		log_error("[%s] (%s) INSERT failed: %s",__FILE_NAME__,
+              __func__, PQerrorMessage(db->conn));
+	PQclear(res);
+
+  pthread_mutex_unlock(&(db->mutex));
+}
+
--- server/src/core/psql_api.h
+++ server/src/core/psql_api.h
@@ -8,41 +8,55 @@
 
 /* ************************************************************************** */
 
+// Create table commands
 #define CRT_PRODUCT_TBL \
     "CREATE TABLE IF NOT EXISTS product(id INTEGER PRIMARY KEY," \
     "name VARCHAR(20), price FLOAT)"
 
 #define CRT_RECEIPT_TBL \
-    "CREATE TABLE IF NOT EXISTS product(id INTEGER PRIMARY KEY," \
+    "CREATE TABLE IF NOT EXISTS receipt(id INTEGER PRIMARY KEY," \
     "date VARCHAR(20), total_price FLOAT)"
+
+// Insert table commands
+#define INSERT_PRODUCT "INSERT INTO product VALUES($1, $2, $3)"
+
+#define INSERT_RECEIPT "INSERT INTO receipt VALUES($1, $2, $3)"
+
+// Result format for PQexecParams
+#define TEXT_FORMAT 0
+#define BINARY_FORMAT 1
+
+// number of parameters for both tables
+#define N_PARAMS 3
+#define ENOUGHT 10 // plenty size for id and price string
+#define PRICE_MAX 100 // max price of a single product
 
 /* ************************************************************************** */
 
-//TODO: Struct per la gestione delle operazioni sul db
-//          PGconn
-//          mutex
-//          condition variable
-//          bool per product già popolata
+typedef struct db_t {
+    pthread_mutex_t mutex;
+    PGconn *conn;
+} db_t;
 
 /* ************************************************************************** */
 
 // Initialization functions
 
-PGconn *init_db(const char*);
+db_t *init_db(const char*);
+
+void destroy_db(db_t *);
 
 void cleanup_db(PGconn*, PGresult*);
 
 bool create_table(PGconn*, const char*);
 
-//TODO: funzione per popolare la tabella PRODUCT
-
-//TODO: funzione per distruggere la struttura
+void populate_product_tbl(db_t *);
 
 /* ************************************************************************** */
 
 // data operations
 
-//TODO: insert_product (non ha bisogno di sincronizzazione)
+void insert(db_t *, const char *, const int, const char *, const float);
 //      insert_receipt (va sincronizzata)
 //      get_all_products (va sincronizzata se la uso nei thread altrimenti)
 //                       (posso chiamarla prima e salvarla nel server)
